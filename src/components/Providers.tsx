82b9f8e4dd09e1c0e4a77316e03bbb37ee014400
danielrenteria23 <daniel.renteria23@gmail.com>
2025-11-23 18:46:03 -0700

    Oops forgot to add my newly created files

---
+++ src/components/Providers.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // Would optimize with more time, but this is demo purposes
+            // staleTime: 60 * 1000,
+            //gcTime: 5 * 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
